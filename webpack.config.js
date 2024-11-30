ead151b6aa706dc19d7733ed7ac85d07186b920e
ilyas <oulkaddailyas@gmail.com>
2024-11-30 17:04:44 +0100

    extension dependencies

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import CopyPlugin from 'copy-webpack-plugin';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const config = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+        background: './src/background/background.js',
+        content: './src/content/content.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js',
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "public",
+                    to: "." // Copies to build folder
+                },
+            ],
+        })
+    ],
+};
+
+export default config
